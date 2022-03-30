1d3d13b0c351869ebf548b13e459463aa7aa846c
ikuzmych <93753692+ikuzmych@users.noreply.github.com>
2022-03-29 17:01:43 -0700

    changed files
    updates to get all proper files in

--- DE1_SoC.sv
+++ DE1_SoC.sv
@@ -4,7 +4,8 @@ module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 		output logic [9:0] LEDR;
 		input logic [3:0] KEY;
 		input logic [9:0] SW;
-
+		input logic CLOCK_50;
+		
 		assign HEX0 = '1;
 		assign HEX1 = '1;
 		assign HEX2 = '1;
@@ -17,6 +18,7 @@ module DE1_SoC_testbench();
 	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	logic [9:0] LEDR;
 	logic [3:0] KEY;
-	input logic [9:0] SW;
+	logic [9:0] SW;
+	logic CLOCK_50;
 
 endmodule
